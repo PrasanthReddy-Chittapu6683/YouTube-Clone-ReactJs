4972e3e9d6516fff8196d184f80dfc7960c69c18
PrasanthReddy-Chittapu6683 <Prasanthreddy.chittapu@gmail.com>
2020-12-25 23:26:31 +0530

    final commit

--- youtube-clone/src/VideoRow.js
+++ youtube-clone/src/VideoRow.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import './VideoRow.css'
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./VideoRow.css";
 
+function VideoRow({
+  views,
+  subs,
+  timestamp,
+  channel,
+  description,
+  title,
+  image,
+  channelImage,
+}) {
+  return (
+    <div className="videoRow">
+      <img className="videoRow__image" src={image} alt="" />
 
-function VideoRow({ views, subs, timestamp, channel,description, title, image }) {
-    return (
-        <div className="videoRow">
-            <img className="videoRow__image" src={image} alt="" />
-
-            <div className="videoRow__text">
-                <h3>
-                    {title}
-                </h3>
-                <p> {channel} . {subs} Subscribers {views} . {timestamp}   </p>
-                <p>{description}</p>
-            </div>
-        </div>
-    )
+      <div className="videoRow__text">
+        <h3>{title}</h3>
+        <p className="videoRow__headline">
+        {views} Views. {timestamp} 
+          <p className="videoRow__channelInfo">
+            <Avatar
+              className="videoRowChannel__logo"
+              alt={channel}
+              src={channelImage}
+            />
+            {channel} .
+          </p>
+          <span className="videoRow__subs">
+            <span className="videoRow__subsNumber"> {subs} </span>
+            Subscribers
+          </span>
+         
+        </p>
+        <p className="videoRow__description"> {description}</p>
+      </div>
+    </div>
+  );
 }
 
-export default VideoRow
+export default VideoRow;
